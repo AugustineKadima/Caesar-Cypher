a8aac99ca80d5dcfaa2b4eeb35954a0629852787
C0d3president <kadimasam@gmail.com>
2021-07-30 23:24:46 +0300

    Test shiftCharacterOneStepForward method for a fail

--- src/main/java/CaesarCypher.java
+++ src/main/java/CaesarCypher.java
@@ -1,2 +1,23 @@
+/*
+CAESAR CYPHER
+char[] alphabets = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+There is a shift of 1 pushing letters forward
+EXPECTED
+Char[] shiftedAlphabet = {'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a'}
+
+METHOD
+Break a string to characters
+Create a characters array
+Use character++ method
+Loop through an array for character array
+Input string
+output character
+Convert output to string
+Join string characters to form words
+If character does not exist, character is empty or character is a punctuation mark do not shift.
+*/
+
 public class CaesarCypher {
 }
--- src/test/java/CaesarCypherTest.java
+++ src/test/java/CaesarCypherTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarCypherTest {
+    @Test
+    public void caesar_Cypher_shiftCharacterOneStepForward_char(){
+        CaesarCypher caesarCypher = new CaesarCypher();
+        char actual = 'a';
+        char expected = 'b';
+        Assertions.assertEquals(expected, caesarCypher.shiftCharOneStepForward(actual));
+    }
 }
