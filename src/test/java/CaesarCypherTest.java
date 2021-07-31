480544c31ed50b7f54a4ce186683b2ad1701cbc5
C0d3president <kadimasam@gmail.com>
2021-07-31 03:15:46 +0300

    Add a condition for an empty string

--- src/test/java/CaesarCypherTest.java
+++ src/test/java/CaesarCypherTest.java
@@ -3,9 +3,15 @@ import org.junit.jupiter.api.Test;
 
 public class CaesarCypherTest {
     @Test
-    public void caesar_Cypher_shiftCharacterOneStepForward_char(){
+    public void caesar_Cypher_shiftCharacterOneStepForward_string_int(){
         CaesarCypher caesarCypher = new CaesarCypher();
         String expected = "a";
         Assertions.assertEquals(expected, caesarCypher.shiftCharOneStepForward("a", 0));
+    }
+    @Test
+    public void caesar_Cypher_shiftCharacterOneStepForward_emptyString_int(){
+        CaesarCypher caesarCypher = new CaesarCypher();
+        String expected = "";
+        Assertions.assertEquals(expected, caesarCypher.shiftCharOneStepForward("", 0));
     }
 }
