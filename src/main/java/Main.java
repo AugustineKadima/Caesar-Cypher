3af80e92241e600da945ae03240c55a5494cad26
C0d3president <kadimasam@gmail.com>
2021-08-01 00:21:01 +0300

    Add decoration to separate encrypted text from the rest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main (String[] args){
         CaesarCypher caesarCypher = new CaesarCypher();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the word you wish to encrypt...");
+        System.out.println("Enter the word you wish to encrypt: ");
         String stringToEncrypt = scanner.nextLine();
-        System.out.println("Enter shift key...");
+        System.out.println("Enter shift key: ");
         int shiftKey = scanner.nextInt();
         String encryptedString = caesarCypher.shiftCharOneStepForward(stringToEncrypt, shiftKey);
+        System.out.println("*************************************");
         System.out.println("Encrypted word: " + encryptedString);
-
+        System.out.println("*************************************");
 
     }
 }
