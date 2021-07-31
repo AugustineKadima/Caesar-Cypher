dfd3f4b42904cf6fdb00200e24e2260d7ff1188d
C0d3president <kadimasam@gmail.com>
2021-07-31 22:05:31 +0300

    Remove comments

--- src/main/java/CaesarCypher.java
+++ src/main/java/CaesarCypher.java
@@ -1,23 +1,3 @@
-/*
-CAESAR CYPHER
-char[] alphabets = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-'l', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-There is a shift of 1 pushing letters forward
-EXPECTED
-Char[] shiftedAlphabet = {'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-'l', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a'}
-
-METHOD
-Break a string to characters
-Create a characters array
-Use character++ method
-Loop through an array for character array
-Input string
-output character
-Convert output to string
-Join string characters to form words
-If character does not exist, character is empty or character is a punctuation mark do not shift.
-*/
 
 public class CaesarCypher {
     int shiftValue;
@@ -63,6 +43,7 @@ public class CaesarCypher {
             }
         return newShiftedString;
     }
+
     public String decipherText(String randomWord, int shiftValue){
 
         this.alphabetWord = randomWord;
